8e88d651932a420efb6d05e97070fa9ea075bb5c
AurionHoward <aurionhoward@gmail.com>
2021-09-22 18:50:01 -0400

    commit tree 84ce7eb8770cf87a1326b9c783fad4d3593c0c6e

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-
 const cats = ["Milo", "Otis", "Garfield"];
-  
-    function destructivelyAppendCat(name) {
-        cats.push("Ralph");
-    }
 
-    function destructivelyPrependCat(name) {
-        cats.unshift("Bob");
-     }
-      
-    function destructivelyRemoveLastCat(name) {
-        cats.pop();
-    }
- 
-    function destructivelyRemoveFirstCat(name) {
-        cats.shift();
-    }
+function destructivelyAppendCat(name){
+cats.push('Ralph');
 
-    function appendCat(name) {
-        const newArray =[...cats]
-        newArray.push(name);
-        return newArray;
-    }
-        
-    function prependCat(name) {
-        return [name, ...cats]
-    }
-     
-    function removeLastCat() {
-        return cats.slice(0, cats.length-1);
-    }
-    
-    function removeFirstCat() {
-        const newArray =cats.slice(1);
-        return newArray;
-    }
-    
+}
 
+function destructivelyPrependCat(name){
+    cats.unshift('Bob');
+
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newArray =[...cats]
+    newArray.push(name);
+    return newArray;
+}
+
+function prependCat(name){
+    return [name,...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat() {
+    const newArray =cats.slice(1);
+    return newArray;
+}
